2e636d7486071cc132573903d11d92d5248ab969
mayank19o7 <mayank.sahu1907@gmail.com>
2024-04-28 23:38:48 +0530

    5. Using Config Server to fetch DB Configurations from GitHub Repository - Book Service (Discovery Client).

--- BookClientService/src/main/java/com/mak/bookclient/repository/BookRepository.java
+++ BookClientService/src/main/java/com/mak/bookclient/repository/BookRepository.java
@@ -5,6 +5,9 @@ import com.mak.bookclient.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for performing CRUD operations on the <code>Book</code> entity.
+ */
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
