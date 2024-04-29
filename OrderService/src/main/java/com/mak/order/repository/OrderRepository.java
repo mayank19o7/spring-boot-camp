b194382af42b3eda38f74555ca809a6e523526ac
mayank19o7 <mayank.sahu1907@gmail.com>
2024-04-30 00:49:32 +0530

    8. Resilience4j - Circuit breaker mechanism - (Order Management Application)

--- OrderService/src/main/java/com/mak/order/repository/OrderRepository.java
+++ OrderService/src/main/java/com/mak/order/repository/OrderRepository.java
@@ -6,8 +6,17 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Repository interface for managing {@link Order} entities.
+ */
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    /**
+     * Find orders by category.
+     *
+     * @param category the category to search for
+     * @return a list of orders matching the given category
+     */
     List<Order> findByCategory(String category);
 }
--- OrderService/src/main/java/com/mak/order/web/OrderController.java
+++ OrderService/src/main/java/com/mak/order/web/OrderController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Controller for managing orders.
+ */
 @RestController
 @RequestMapping("orders")
 public class OrderController {
@@ -17,11 +20,22 @@ public class OrderController {
     @Autowired
     OrderService orderService;
 
+    /**
+     * Retrieve all orders.
+     *
+     * @return a list of all orders
+     */
     @GetMapping
     public List<Order> getAllOrders() {
         return orderService.getAllOrders();
     }
 
+    /**
+     * Retrieve orders by category.
+     *
+     * @param category the category to filter orders by
+     * @return a list of orders in the specified category
+     */
     @GetMapping("{category}")
     public List<Order> getOrders(@PathVariable String category) {
         return orderService.getOrders(category);
